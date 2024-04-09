ededfc27546f9606b79b702e0988a15e4ed6b834
blaze <blazezhang@liontravel.com>
2024-04-09 12:13:20 +0800

    加入專案檔案。

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace CoreMVC_SignalR_Chat.Hubs
+{
+    public class ChatHub: Hub
+    {
+        // 用戶連線 ID 列表
+        public static List<string> ConnIDList = new List<string>();
+
+        /// <summary>
+        /// 連線事件
+        /// </summary>
+        /// <returns></returns>
+        public override async Task OnConnectedAsync()
+        {
+
+            if (ConnIDList.Where(p => p == Context.ConnectionId).FirstOrDefault() == null)
+            {
+                ConnIDList.Add(Context.ConnectionId); //用戶識別碼重給予
+            }
+
+            // 更新個人 ID
+            await Clients.Client(Context.ConnectionId).SendAsync("UpdSelfID", Context.ConnectionId);
+
+            // 更新連線 ID 列表
+            string jsonString = JsonConvert.SerializeObject(ConnIDList);
+            await Clients.All.SendAsync("UpdList", jsonString);
+
+            
+
+            // 更新聊天內容
+            await Clients.All.SendAsync("UpdContent", $"{Context.ConnectionId}已加入");
+
+            await base.OnConnectedAsync();
+        }
+
+        /// <summary>
+        /// 離線事件
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            string id = ConnIDList.Where(p => p == Context.ConnectionId).FirstOrDefault();
+            if (id != null)
+            {
+                ConnIDList.Remove(id);
+            }
+            // 更新連線 ID 列表
+            string jsonString = JsonConvert.SerializeObject(ConnIDList);
+            await Clients.All.SendAsync("UpdList", jsonString);
+
+            // 更新聊天內容
+            await Clients.All.SendAsync("UpdContent", $"{Context.ConnectionId}已離開");
+
+            await base.OnDisconnectedAsync(ex);
+        }
+
+        /// <summary>
+        /// 加入群組
+        /// </summary>
+        /// <param name="groupName"></param>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        public async Task AddGroup(string groupName, string username)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("RecAddGroupMsg", $"{username} 已加入 群組：{groupName}。");
+        }
+
+        /// <summary>
+        /// 入群組訊息告知
+        /// </summary>
+        /// <param name="groupName"></param>
+        /// <param name="username"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public Task SendMessageToGroup(string groupName, string username, string message)
+        {
+            return Clients.Group(groupName).SendAsync("ReceiveGroupMessage", groupName, username, message);
+        }
+
+        /// <summary>
+        /// 傳遞訊息
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="message"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task SendMessage(string selfID, string message, string sendToID)
+        {
+            if (string.IsNullOrEmpty(sendToID))
+            {
+                await Clients.All.SendAsync("UpdContent", selfID + " 說: " + message);
+            }
+            else
+            {
+                // 接收人
+                await Clients.Client(sendToID).SendAsync("UpdContent", selfID + " 私訊你: " + message);
+
+                // 發送人
+                await Clients.Client(Context.ConnectionId).SendAsync("UpdContent", "你 " + sendToID + " 私訊: " + message);
+            }
+        }
+    }
+}
